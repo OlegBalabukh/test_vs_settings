509f31e61a1bd797f917890759e3c68147632e7a
obalab <obalab@softserveinc.com>
2019-06-27 13:55:00 +0300

    added functions add, avg, max, min

--- index.js
+++ index.js
@@ -17,3 +17,36 @@ let multipleTwoTimes = () => {
   })
 }
 
+// sum all nums
+function add() {  
+  var args = Array.prototype.slice.call(arguments);
+  return args.reduce((a,b) => a + b, 0);  
+}
+add(2, 3, 4, 5); // 14
+
+// or
+function add(...args) {
+  return args.reduce((a,b) => a + b, 0);  
+}
+add(2, 3, 4, 5); // 14
+
+function avg(...args) {
+  return args.reduce((a,b) => a + b, 0) / args.length; 
+}
+avg(2, 3, 4, 5); // 3.5
+
+function max(...args) {
+  return args.reduce((a,b) => a < b ? b : a); 
+}
+max(2, 7, 3, 4, 5); // 7
+
+function min(...args) {
+  return args.reduce((a,b) => a < b ? a : b); 
+}
+min(2, -1, 7, 3, 4, 5, 1); // -1
+// if args is an array
+function min(arr) {
+  return arr.reduce((a,b) => a < b ? a : b); 
+}
+min(2, -1, 7, 3, 4, 5, 1); // -1
+
